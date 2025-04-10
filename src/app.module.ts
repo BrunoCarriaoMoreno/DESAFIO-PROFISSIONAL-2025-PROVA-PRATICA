fbc3d4da3df53a343ed0bdf12af24a3d65aafa8b
Bruno <brunobatistacarriaomoreno@gmail.com>
2025-04-10 20:20:53 -0300

    criação da entidade personagem

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersonagemModule } from './personagem/personagem.module';
+import { Mongoose } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/personagem-db'),
+    PersonagemModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('RPG API')
+  .setDescription('API para gerenciamento de um jogo estilo RPG')
+  .setVersion('1.0')
+  .addTag('personagem')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
