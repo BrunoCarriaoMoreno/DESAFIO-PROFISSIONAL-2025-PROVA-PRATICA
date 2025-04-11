76a1c106092dde30a605d16ee9c9d2f300f9bfd8
Bruno <brunobatistacarriaomoreno@gmail.com>
2025-04-11 19:32:24 -0300

    criação das rotas do personagem

--- src/personagem/personagem.controller.ts
+++ src/personagem/personagem.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, Get, Param, Patch, Delete } from '@nestjs/common';
+import { PersonagemService } from './personagem.service';
+import { CreatePersonagemDto } from './dto/create-personagem.dto';
+import { Personagem } from './schemas/personagem.schema';
+import { UpdateNomeAventureiroDto } from './dto/update-personagem.dto';
 
 @Controller('personagem')
-export class PersonagemController {}
+export class PersonagemController {
+    constructor(private readonly personagemService: PersonagemService) {}
+
+    @Post()
+    create(@Body() dto: CreatePersonagemDto) {
+        return this.personagemService.create(dto);
+    }
+
+    @Get()
+    async findAll(): Promise<Personagem[]> {
+        return this.personagemService.findAll();
+    }
+
+    @Get(':id')
+    async findById(@Param('id') id: string): Promise<Personagem>{
+        return this.personagemService.findById(id);
+    }
+
+    @Patch(':id/nome-aventureiro')
+    async updateNomeAventureiro(
+        @Param('id') id: string,
+        @Body() body: UpdateNomeAventureiroDto,
+    ): Promise<Personagem> {
+        return this.personagemService.updateNomeAventureiro(id, body.nomeAventureiro);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: string): Promise<void> {
+        return this.personagemService.remove(id);
+    }
+}
--- src/personagem/personagem.service.ts
+++ src/personagem/personagem.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Personagem, PersonagemDocument } from './schemas/personagem.schema';
+import { CreatePersonagemDto } from './dto/create-personagem.dto';
 
 @Injectable()
-export class PersonagemService {}
+export class PersonagemService {
+    constructor(
+        @InjectModel(Personagem.name)
+        private readonly personagemModel: Model<Personagem>
+    ) {}
+
+    async create(dto: CreatePersonagemDto): Promise<Personagem> {
+        const totalPontos = dto.forca + dto.defesa;
+      
+        if (totalPontos > 10) {
+          throw new BadRequestException('A soma de força e defesa não pode passar de 10 pontos.');
+        }
+      
+        const novoPersonagem = new this.personagemModel({
+          ...dto,
+          itensMagicos: [],
+        });
+      
+        return await novoPersonagem.save();
+      }
+
+      async findAll(): Promise<Personagem[]> {
+        return this.personagemModel.find().exec();
+      }
+
+      async findById(id: string): Promise<Personagem> {
+        const personagem = await this.personagemModel.findById(id).exec();
+
+        if (!personagem) {
+            throw new NotFoundException(`Personagem com ${id} não encontrado`);
+        }
+
+        return personagem;
+      }
+
+      async updateNomeAventureiro(id: string, nomeAventureiro: string): Promise<Personagem> {
+        const personagem = await this.personagemModel.findByIdAndUpdate(
+            id,
+            { nomeAventureiro },
+            { new: true }
+        );
+
+        if(!personagem) {
+            throw new NotFoundException(`Personagem com ID ${id} não encontrado`);
+        }
+
+        return personagem;
+      }
+
+      async remove(id: string): Promise<void> {
+        const result = await this.personagemModel.findByIdAndDelete(id);
+
+        if(!result) {
+            throw new NotFoundException(`Personagem com ID ${id} não encontrado`)
+        }
+      }
+    }
